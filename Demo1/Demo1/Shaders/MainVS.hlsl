669e7ca5026e3e8607f058398e3868ea919e4b93
Álvaro Tomás Lozano <auberontermeraz@gmail.com>
2020-01-12 22:29:00 +0100

    Demo 1 initial commit.

---
+++ Demo1/Demo1/Shaders/MainVS.hlsl
@@ -0,0 +1,25 @@
+#include "MainShared.hlsl"
+#include "Utils.hlsl"
+
+struct InputVS
+{
+	float4 position : POSITION;
+	#ifdef VERTEX_COLOR
+	float4 color : COLOR;
+	#endif
+};
+
+OutputVS main( InputVS input )
+{
+	float3 offset = offsetScale.xyz;
+	float scale = offsetScale.w;
+
+	OutputVS output;
+	output.position = input.position;
+	output.position.xy *= scale;
+	output.position.xyz += offset;
+	#ifdef VERTEX_COLOR
+	output.color = input.color;
+	#endif
+	return output;
+}
